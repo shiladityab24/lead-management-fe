d578279bf72be84637aa92fbdd190ef2de07de30
shiladityab24 <shiladityab24@yahoo.com>
2024-10-07 03:07:21 +0530

    Added heroku be-url

--- src/modules/pages/login/login.js
+++ src/modules/pages/login/login.js
@@ -1,5 +1,5 @@
 import {LightningElement } from 'lwc';
-const BACKEND_URL = 'https://lead-management-be.onrender.com'||'http://localhost:3002'
+const BACKEND_URL = 'https://lead-management-be-db0bd8b494eb.herokuapp.com' ||'https://lead-management-be.onrender.com'||'http://localhost:3002'
 export default class Login extends LightningElement{
     get loginUrl(){
         return `${BACKEND_URL}/oauth2/login`
